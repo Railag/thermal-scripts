504ce76f692e131219565ca38cd126b4694b738f
Railag <whenthegroundcavedin@gmail.com>
2019-04-08 13:37:28 +0300

    Notebook FanControl wrapper (enables/disables 3rd fan)

---
+++ 3rdfan.ps1
@@ -0,0 +1,28 @@
+﻿param (
+	[switch]$status,
+	[switch]$enable,
+	[switch]$disable,
+	[switch]$auto
+)
+
+$statusCommand = "nbfc.exe status -a";
+$enable3rdFanCommand = "nbfc.exe set 100 -f 2";
+$disable3rdFanCommand = "nbfc.exe set 0 -f 2";
+$auto3rdFanCommand = "nbfc.exe set auto -f 2";
+
+if ($status)
+{
+	Invoke-Expression $statusCommand
+}
+elseif ($enable)
+{
+	Invoke-Expression $enable3rdFanCommand
+}
+elseif ($disable)
+{
+	Invoke-Expression $disable3rdFanCommand
+}
+elseif ($auto)
+{
+	Invoke-Expression $auto3rdFanCommand
+}
